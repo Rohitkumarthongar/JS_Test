4a6aef94feabb62419dc51936ee2a6f205da1a97
rohit_kumar_soni <rohitkumarsoni713@gmail.com>
2024-10-01 23:42:34 +0530

    update

---
+++ demo.js
@@ -0,0 +1,76 @@
+//OPerators 
+
+let userInput = prompt("Please enter your input:");
+console.log("User input is:", userInput);
+
+
+let A = 8
+B = 5
+// console.log(A+B) //13
+
+
+
+// c = ++A
+// console.log(c)
+
+
+// B = 9add.export
+//Logic Operators
+
+// if (A == B)
+// {
+
+//     console.log(A+"-----" +B+"    "   +"A is greater than B")
+// }
+// else
+// {
+//     console.log(A+" -----  " +B+"------------"+"B is greater than A")
+// }
+
+
+//Function 
+
+// function add(a,b)
+// {
+//     return a+b
+// }
+// console.log(add(A,B))
+
+//Array 
+
+
+cric = [7, 6, 4, 45]
+//Loops
+//For Loop
+
+// for(let i=0;i<=cric.length();i++)
+// {
+//     console.log(i.valueOf(i))
+// }
+
+days = ["Sunday", "Monday", "Friday", "Saturday"]
+
+party = "Sunday"
+
+
+// for (let i = 0; i < days.length; i++) {
+//     // switch (party) {
+//     //     case ("Monday"):
+//     //         console.log("Today is Monday, Party Koni mil")
+//     //         break;
+
+//     //     case ("Friday"):
+//     //         console.log("Today is Friday, Party Koni mil")
+//     //         break
+
+//     //     case ("Sunday"):
+//     //         console.log("Today is Sunday,Ab to Party LEvo La")
+//     //         break
+//     // }
+//     if (days[i] == "Monday")
+//         {
+//             console.log("Today is Friday, Party Koni mil")
+//             break
+//         }else{
+//             console.log(" Party Koni mil")
+//         }
